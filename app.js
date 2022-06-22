83d17a9e3be7e0e3b575fa8274441841233941bc
JasonChan <jasonchan@jubo.health>
2022-06-22 21:30:21 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
+const session = require("express-session");
+
+const routes = require("./routes/index");
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
+app.set("view engine", "hbs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(routes);
+
+app.listen(port, () => console.log(`server is listening on localhost:${port}`));
