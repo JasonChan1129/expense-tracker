20555b5ceff857cc52999ac912f9b8433a544c7b
Jason Chan <jasonchan19981017@hotmail.com>
2022-06-23 23:44:27 +0800

    feat: create index page view

---
+++ public/icon.js
@@ -0,0 +1,9 @@
+const CATEGORY = {
+	家居物業: '<i class="fas fa-house-user fa-lg text-info"></i>',
+	交通出行: '<i class="fas fa-shuttle-van fa-lg text-info"></i>',
+	休閒娛樂: '<i class="fas fa-grin-beam fa-lg text-info"></i>',
+	餐飲食品: '<i class="fas fa-utensils fa-lg text-info"></i>',
+	其他: '<i class="fs fa-pen fa-lg text-info"></i>',
+};
+
+module.exports = CATEGORY;
