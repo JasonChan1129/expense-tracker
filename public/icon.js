22976813bb86150519a57ef6311dedd82c54df6b
Jason Chan <jasonchan19981017@hotmail.com>
2022-06-25 22:46:12 +0800

    modify: modify icon link

--- public/icon.js
+++ public/icon.js
@@ -3,7 +3,7 @@ const CATEGORY = {
 	交通出行: '<i class="fas fa-shuttle-van fa-lg text-info"></i>',
 	休閒娛樂: '<i class="fas fa-grin-beam fa-lg text-info"></i>',
 	餐飲食品: '<i class="fas fa-utensils fa-lg text-info"></i>',
-	其他: '<i class="fs fa-pen fa-lg text-info"></i>',
+	其他: '<i class="fas fa-pen fa-lg text-info"></i>',
 };
 
 module.exports = CATEGORY;
