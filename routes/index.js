306d506c3b2cd16170c5318738dac99d9d0c1c09
Jason Chan <jasonchan19981017@hotmail.com>
2022-06-25 23:54:31 +0800

    feat: add auth middleware

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,11 @@ const home = require('./modules/home');
 const records = require('./modules/records');
 const users = require('./modules/users');
 
+// auth middleware
+const { authenticator } = require('../middleware/auth');
+
 router.use('/users', users);
-router.use('/records', records);
-router.use('/', home);
+router.use('/records', authenticator, records);
+router.use('/', authenticator, home);
 
 module.exports = router;
