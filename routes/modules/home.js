3d44846ecb9f3fe2c2e91980153bf59e0b693df5
Jason Chan <jasonchan19981017@hotmail.com>
2022-06-27 20:42:48 +0800

    refactor: change code in filter function to async await

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -28,30 +28,33 @@ router.get('/', (req, res) => {
 		.catch(err => console.log(err));
 });
 
-router.post('/filter', (req, res) => {
+router.post('/filter', async (req, res) => {
 	const filter = req.body.select;
 	const userId = req.user._id;
-	Category.findOne({ name: filter })
-		.then(category => {
-			Record.find({ categoryId: category._id, userId })
-				.lean()
-				.then(records => {
-					let totalAmount = 0;
-					records.forEach(record => {
-						// transform ISO date to YYYY - MM - DD
-						let date = new Date(record.date);
-						date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-						record.date = date;
-						// get the icon HTML
-						record.icon = icons[category.name];
-						// add total
-						totalAmount += record.amount;
-					});
-					res.render('index', { records, totalAmount });
-				})
-				.catch(err => console.log(err));
-		})
-		.catch(err => console.log(err));
+	let totalAmount = 0;
+
+	if (filter === '全部') {
+		return res.redirect('/');
+	}
+	try {
+		const category = await Category.findOne({ name: filter }).lean();
+		const records = await Record.find({ categoryId: category._id, userId }).lean();
+
+		records.forEach(record => {
+			// transform ISO date to YYYY - MM - DD
+			let date = new Date(record.date);
+			date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+			record.date = date;
+			// get the icon HTML
+			record.icon = icons[category.name];
+			// add total
+			totalAmount += record.amount;
+		});
+
+		res.render('index', { records, totalAmount, category: category.name });
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 module.exports = router;
